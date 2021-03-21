b47d39a97affdc23c45593878489967c767f489d
KhaledAlawartani <70545735+KhaledAlawartani@users.noreply.github.com>
2021-03-21 18:18:09 -0400

    Finished today's assignment

---
+++ Module_7/hideimg.js
@@ -0,0 +1,7 @@
+function hide(){
+    document.getElementById('logo').className = "hidden";
+    document.getElementById('crosby').className = "hidden";
+    document.getElementById('malkin').className = "hidden";
+    document.getElementById('rust').className = "hidden";
+    document.getElementById('letang').className = "hidden";
+  }
